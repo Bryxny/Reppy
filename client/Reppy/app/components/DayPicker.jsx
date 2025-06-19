2d1e18bcea2a696c6c77c5f8018995cf6824ae08
Bryxny <bryonydoh2@gmail.com>
2025-06-19 11:48:56 +0100

    working on app layout

---
+++ client/Reppy/app/components/DayPicker.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Text, View } from "react-native";
+
+export default function DayPicker() {
+  const daysOfWeek = {
+    Monday: false,
+    Tuesday: false,
+    Wednesday: false,
+    Thursday: false,
+    Friday: false,
+    Saturday: false,
+    Sunday: false,
+  };
+}
--- client/Reppy/app/index.jsx
+++ client/Reppy/app/index.jsx
@@ -1,6 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, StyleSheet, Button } from "react-native";
+import { useState } from "react";
+import { useUser } from "./context/UserContext";
+import { router } from "expo-router";
 
 export default function Index() {
+  const { name, setName } = useUser();
+  const [nameInput, setNameInput] = useState("");
+  const [error, setError] = useState("");
+
+  async function handleLogin() {
+    if (nameInput.trim()) {
+      await setName(nameInput.trim());
+      setError("");
+      router.navigate("/generator");
+    } else {
+      setError("Please enter your name");
+    }
+  }
+
   return (
     <View
       style={{
@@ -9,7 +26,24 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>What's your name?</Text>
+      <TextInput
+        style={styles.input}
+        value={nameInput}
+        onChangeText={setNameInput}
+        autoFocus
+      />
+      <Button title="submit" onPress={handleLogin} />
+      {error && <Text>{error}</Text>}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
