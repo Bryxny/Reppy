7137b919c12877f0b1ddc76bbedd09d9410ef550
Bryxny <bryonydoh2@gmail.com>
2025-07-09 21:03:42 +0100

    feat: styled home

--- client/Reppy/app/(tabs)/_layout.jsx
+++ client/Reppy/app/(tabs)/_layout.jsx
@@ -1,20 +1,55 @@
 import { Tabs } from "expo-router";
+import { FontAwesome5, FontAwesome } from "@expo/vector-icons";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 20,
+          width: 350,
+          transform: [{ translateX: 25 }],
+          backgroundColor: "#29272f",
+          borderRadius: 20,
+          height: 60,
+          borderTopWidth: 0,
+          paddingTop: 10,
+          paddingBottom: 0,
+        },
+        tabBarIconStyle: {
+          marginTop: 0,
+          alignSelf: "center",
+        },
+        tabBarShowLabel: false,
         headerShown: false,
       }}
     >
-      <Tabs.Screen name="home" />
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: () => (
+            <FontAwesome name="home" size={26} color="#a5aacc" />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="workout"
         options={{
           tabBarStyle: { display: "none" },
+          tabBarIcon: () => (
+            <FontAwesome5 name="dumbbell" size={22} color="#b0cca5" />
+          ),
         }}
       />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: () => (
+            <FontAwesome name="user-circle-o" size={22} color="#f9eaa1" />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
--- client/Reppy/components/Calender.jsx
+++ client/Reppy/components/Calender.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { useToday } from "../context/TodayContext";
+import { FontAwesome, AntDesign } from "@expo/vector-icons";
 const dayLabels = {
   sunday: "S",
   monday: "M",
@@ -10,21 +10,43 @@ const dayLabels = {
   friday: "F",
   saturday: "S",
 };
+
+const bgColors = [
+  "#a5aacc",
+  "#b0cca5",
+  "#f9eaa1",
+  "#a5aacc",
+  "#b0cca5",
+  "#f9eaa1",
+  "#a5aacc",
+];
+
 export default function Calender() {
   const { todaysPlan, todaysKey, loading } = useToday();
 
   if (loading) return <Text>Loading...</Text>;
   return (
-    <View>
-      {Object.entries(dayLabels).map(([day, label]) => {
+    <View
+      className="flex-row justify-center gap-5 bg-black p-7 rounded-3xl"
+      style={{
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 6,
+      }}
+    >
+      {Object.entries(dayLabels).map(([day, label], i) => {
         return (
           <View key={day}>
-            <Text>{label}</Text>
-            <View
-              style={[
-                styles.circle,
-                day === todaysKey ? styles.circleActive : styles.circleInactive,
-              ]}
+            <Text className="text-3xl font-bold mb-3 text-grey text-center">
+              {label}
+            </Text>
+
+            <FontAwesome
+              name={day === todaysKey ? "circle" : "circle-o"}
+              size={28}
+              color={bgColors[i]}
             />
           </View>
         );
@@ -32,19 +54,3 @@ export default function Calender() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  circle: {
-    width: 16,
-    height: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: "#000",
-  },
-  circleActive: {
-    backgroundColor: "#000",
-  },
-  circleInactive: {
-    backgroundColor: "transparent",
-  },
-});
